9123b04ff7cfc7afd39ffb7a266d8d758c1db6ef
pushhhkar <140235997+pushhhkar@users.noreply.github.com>
2025-01-12 10:56:24 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 144553 //can't change further
+let accountEmail = "pushkar@google.com"
+var accountPassword = "12345"
+accountCity = "pushkar"
+let accountState;
+//accountId = 2 // not allowed
+
+accountEmail = "pk@pk.com"
+accountPassword = "21212121"
+accountCity = "Benagaluru"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";// treat all js code as neewer version
+
+// alert( 3 + 3)//it will show error beacause we are using nodejs,not browser
+console.log( 3 + 3) //code readebility should be high
+console.log("pushkar")
+let name = "pushkar"
+let age = 18
+let isLoggedIn = false
+//data types
+//number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+// null=> standalone value
+//undefined => defined but not given value
+//symbol => when we talk about unique
+//object
+console.log(typeof "pushkar")  //it tella about data type
