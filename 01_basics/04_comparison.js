49fe7a67def459a2dfe1d8893879914166918050
pushhhkar <140235997+pushhhkar@users.noreply.github.com>
2025-03-07 02:44:35 +0000

    commit4

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2<1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // comparison operator convert null to number treating it as 0.
+
+// === (strictly check)
+console.log("2"===1);
+
+
