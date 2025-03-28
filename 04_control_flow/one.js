beaf85bf2bc40e30cb5407afdcd2b8b7380ddc12
pushhhkar <140235997+pushhhkar@users.noreply.github.com>
2025-03-28 11:47:34 +0000

    controlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// if 
+const isUserloggedIn = true
+const temperature = 41
+
+if(temperature < 50){
+    console.log("less than 50")
+}else {
+    console.log("temperature is greater than 50");
+}
+
+if (2 == "2") {
+    console.log("executed");
+}
+if (2 ===  "2") {
+    console.log("executed");
+}
+
+//<, >, <=, ==, !=, ===
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    console.log(`user power: ${power}`);
+}
+
+const balance = 1000
+
+if(balance > 500) console.log("test");
